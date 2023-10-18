c56b535bed44dbad8254c9670e71c99d0dea2f2e
iamgromov <iamgromov@gmail.com>
2023-10-18 16:51:03 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import Tooltip from "./tooltip/Tooltip";
+
+const button = document.querySelector(".tooltip-btn");
+const text =
+  "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.";
+const tooltip = new Tooltip(button, text);
